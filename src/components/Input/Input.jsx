33938324bbd88070feaf24efbc799bfdd7711b32
AMarchmarti <marchmarti.antoni@gmail.com>
2021-09-04 19:44:59 +0200

    prettier

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,16 +3,10 @@ import React from 'react';
 const Input = ({ placeholder, type, label, handleChange, value }) => {
 	return (
 		<label>
-			<input
-				type={type}
-				onChange={handleChange}
-				placeholder={placeholder}
-				value={value}
-			/>
+			<input type={type} onChange={handleChange} placeholder={placeholder} value={value} />
 			<span>{label}</span>
 		</label>
 	);
 };
-
 
 export default Input;
